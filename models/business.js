0529a7aa16d5d3f288865d51f90b777823d77c56
JungCheChang <jungche0408@gmail.com>
2024-05-02 11:13:46 -0700

    add business model

---
+++ models/business.js
@@ -0,0 +1,18 @@
+const { DataTypes } = require("sequelize")
+const sequelize = require("../sequelize")
+
+const Business = sequelize.define('business', {
+    ownerid: { type: DataTypes.INTEGER, allowNull: false},
+    name : { type: DataTypes.STRING, allowNull: false},
+    address: {type: DataTypes.STRING, allowNull: false},
+    city: {type: DataTypes.STRING, allowNull: false},
+    state: {type: DataTypes.STRING(2), allowNull: false},
+    zip: {type: DataTypes.STRING, allowNull: false},
+    phone: {type: DataTypes.STRING, allowNull: false},
+    category: {type: DataTypes.STRING, allowNull: false},
+    subcategory: {type: DataTypes.STRING, allowNull: false},
+    website: {type: DataTypes.STRING, allowNull: true},
+    email: {type: DataTypes.STRING, allowNull: true}
+})
+
+module.exports = Business
