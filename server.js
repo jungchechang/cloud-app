7a017a7b97bee589acf3212f19944565d5711211
JungCheChang <jungche0408@gmail.com>
2024-05-03 13:15:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 const sequelize = require("./sequelize")
+const Business = require("./models/business")
+const Photo = require('./models/photo')
+const Review = require('./models/review')
+const businesses = require('./data/businesses.json')
+const reviews = require('./data/reviews.json')
+const photos = require('./data/photos.json')
 const api = require('./api')
 
 const app = express()
 const port = process.env.PORT || 8000
-
 /*
  * Morgan is a popular request logger.
  */
@@ -13,6 +18,29 @@ app.use(morgan('dev'))
 
 app.use(express.json())
 app.use(express.static('public'))
+
+//initializeDatabase
+async function initializeDatabase() {
+  try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+
+      // Force true will drop the table if it already exists
+      await sequelize.sync({ force: true });
+      console.log('Tables has been successfully created.');
+      await Business.bulkCreate(businesses);
+      console.log('Business data has been successfully created.');
+      await Review.bulkCreate(reviews);
+      console.log('Review data has been successfully created.');
+      await Photo.bulkCreate(photos);
+      console.log('Photo data has been successfully created.');
+
+  } catch (error) {
+      console.error('Error during the initialization of the database:', error);
+  }
+}
+
+initializeDatabase();
 
 app.use('/', api)
 
@@ -33,12 +61,12 @@ app.use('*', function (err, req, res, next) {
   })
 })
 
-// app.listen(port, function() {
-//   console.log("== Server is running on port", port)
-// })
-
-sequelize.sync().then(function () {
-    app.listen(port, function () {
-        console.log("== Server is listening on port:", port)
-    })
+app.listen(port, function() {
+  console.log("== Server is running on port", port)
 })
+
+// sequelize.sync().then(function () {
+//     app.listen(port, function () {
+//         console.log("== Server is listening on port:", port)
+//     })
+// })
