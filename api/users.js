7dbea70ca706a6d458fd554446a1330075435c40
JungCheChang <jungche0408@gmail.com>
2024-05-19 22:55:31 -0700

    Add authorization and authentication

--- api/users.js
+++ api/users.js
@@ -1,55 +1,148 @@
 const router = require('express').Router()
-
 exports.router = router
-
-const { businesses } = require('./businesses')
-const { reviews } = require('./reviews')
-const { photos } = require('./photos')
-const Business = require("../models/business")
-const Photo = require('../models/photo')
-const Review = require('../models/review')
+const bcrypt = require('bcryptjs')
+const {Business} = require("../models/business")
+const {Photo} = require('../models/photo')
+const {Review} = require('../models/review')
+const {User, getUserByEmail, getUserById, validateCredentials} = require('../models/user')
+const{ generateAuthToken, requireAuthentication, checkAdmin} = require('../lib/auth')
 
 /*
  * Route to list all of a user's businesses.
  */
-router.get('/:userId/businesses',async function (req, res) {
+router.get('/:userId/businesses', requireAuthentication, async function (req, res) {
   const userId = parseInt(req.params.userId)
-  const userBusinesses = await Business.findAll({
-    where:{
-      ownerId: userId
+  if (req.user !== userId && !req.isAdmin) {
+    res.status(403).send({
+      error: "Not authorized to access the specified resource"
+    })
+  } else {
+    try {
+      const userBusinesses = await Business.findAll({
+        where:{
+          ownerId: userId
+        }
+      })
+      res.status(200).send({
+        businesses: userBusinesses
+      })
+    } catch (e) {
+      next(e)
     }
-  })
-  res.status(200).send({
-    businesses: userBusinesses
-  })
+  }
 })
 
 /*
  * Route to list all of a user's reviews.
  */
-router.get('/:userId/reviews', async function (req, res) {
+router.get('/:userId/reviews', requireAuthentication, async function (req, res) {
   const userId = parseInt(req.params.userId)
-  const userReviews = await Review.findAll({
-    where: {
-      userId: userId
+  if (req.user !== userId && !req.isAdmin) {
+    res.status(403).send({
+      error: "Not authorized to access the specified resource"
+    })
+  }else{
+    try{
+      const userReviews = await Review.findAll({
+        where: {
+          userId: userId
+        }
+      })
+      res.status(200).send({
+        reviews: userReviews
+      })
+    }catch(e){
+      next(e)
     }
-  })
-  res.status(200).send({
-    reviews: userReviews
-  })
+  }
 })
 
 /*
  * Route to list all of a user's photos.
  */
-router.get('/:userId/photos', async function (req, res) {
+router.get('/:userId/photos', requireAuthentication, async function (req, res) {
   const userId = parseInt(req.params.userId)
-  const userPhotos = await Photo.findAll({
-    where: {
-      userId: userId
+  if (req.user !== userId && !req.isAdmin) {
+    res.status(403).send({
+      error: "Not authorized to access the specified resource"
+    })
+  }else{
+    try{
+      const userPhotos = await Photo.findAll({
+        where: {
+          userId: userId
+        }
+      })
+      res.status(200).send({
+        photos: userPhotos
+      })
+    }catch(e){
+      next(e)
     }
-  })
-  res.status(200).send({
-    photos: userPhotos
-  })
+  }
+  
+})
+
+/*
+ * Route to create a new user
+ */
+router.post('/', checkAdmin, async function (req, res, next){
+  const user = req.body
+  const hash = await bcrypt.hash(user.password, 8)
+  const userData = {
+    name: user.name,
+    email: user.email,
+    password: hash
+  }
+  if (req.isAdmin){
+    userData.admin  = user.admin
+  }
+  if ( await getUserByEmail(userData.email)){
+    res.status(409).send("User already exit!")
+  }else{
+    try{
+      const user = await User.create(userData)
+      res.status(201).send({id:user.id})
+    }catch(e){
+      next(e)
+    }
+  }
+})
+
+router.post('/login', async function(req, res, next) {
+  const user = req.body
+  try{
+    const authenticated_user = await validateCredentials(user.email, user.password)
+    if (authenticated_user){
+      const token = generateAuthToken(authenticated_user)
+      res.status(200).send({token:token})
+    }else{
+      res.status(401).send({
+        error: "Invalid authentication credentials"
+    })
+    }
+  }catch(e){
+    next(e)
+  }
+  
+})
+
+router.get('/:id', requireAuthentication, async function (req, res, next) {
+  const userId = parseInt(req.params.id)
+  if (req.user !== userId && !req.isAdmin) {
+    res.status(403).send({
+      error: "Not authorized to access the specified resource"
+    })
+  } else {
+      try {
+          const user = await getUserById(userId)
+          if (user) {
+              res.status(200).send(user)
+          } else {
+              next()
+          }
+      } catch (e) {
+          next(e)
+      }
+  }
 })
